8d039ae93106273b7a538f5c32a13483b8212007
Kazemabadi Farahani, Pouria <P.Kazemabadi@sadad.co.ir>
2024-07-26 17:12:41 +0330

    feat:add query client and use loading and isloading

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,21 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data } = useQuery("Super-Heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+
+  if (isLoading) {
+    return <h2>Loading ...</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((item) => (
+        <h3 key={item.name}>{item.name}</h3>
+      ))}
+    </>
+  );
+};
