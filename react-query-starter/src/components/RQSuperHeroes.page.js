78064e624e318681f6d32ef2993d5530a639a372
Kazemabadi Farahani, Pouria <P.Kazemabadi@sadad.co.ir>
2024-07-26 17:34:50 +0330

    feat : add cachTime and StaleTime and understand syntax for error by react query

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,13 +1,29 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/susperheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("Super-Heroes", () => {
-    return axios.get("http://localhost:4000/superheroes");
-  });
+  const { isLoading, data, error, isError, isFetching } = useQuery(
+    "Super-Heroes",
+    fetchSuperHeroes,
+    {
+      // staleTime: 0, By default, this value is always set to zero so that the React Query value is updated every time the page is called.
+      cacheTime: 5000,
+      staleTime: 3000,
+    }
+  );
+
+  // console.log(isLoading, isFetching);
 
   if (isLoading) {
     return <h2>Loading ...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
